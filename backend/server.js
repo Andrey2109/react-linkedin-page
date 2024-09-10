689929c40fff9251d3e4951fe9bbd1fda9d69d0d
Andrey2109 <python2109@gmail.com>
2024-09-10 11:09:56 +0300

    Secure application by externalizing credentials using dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,37 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { Pool } = require("pg");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
-// List of allowed origins
-const allowedOrigins = [
-  "http://site.talentsplease.s3-website-us-west-2.amazonaws.com",
-  "https://d1blze4d54x3w2.cloudfront.net",
-];
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",");
 
-// CORS middleware configuration
 const corsOptions = {
   origin: function (origin, callback) {
-    // allow requests with no origin (like mobile apps or curl requests)
     if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) !== -1) {
+    if (allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
   },
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions)); // Use CORS middleware with options
+app.use(cors(corsOptions));
 
 const pool = new Pool({
-  host: "node128.codingbc.com",
-  database: "postgres",
-  user: "postgres",
-  password: "Dec2017890#",
-  port: 7892,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
-app.use(express.json()); // Middleware for parsing JSON bodies
+app.use(express.json());
 
-// Route to fetch all profiles
 app.get("/profiles", async (req, res) => {
   try {
     const { rows } = await pool.query("SELECT * FROM profiles_example");
